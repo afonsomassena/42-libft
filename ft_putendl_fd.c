a6686d18cea5099fd6f59409b7156f6060d8a6a8
Afonso Massena <afonsomassena@Afonsos-MacBook-Air.local>
2022-11-21 16:16:43 +0000

    hmm

--- ft_putendl_fd.c
+++ ft_putendl_fd.c
@@ -6,7 +6,7 @@
 /*   By: afgoncal <massenaafonso1@gmail.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/21 15:21:52 by afgoncal          #+#    #+#             */
-/*   Updated: 2022/11/21 15:31:40 by afgoncal         ###   ########.fr       */
+/*   Updated: 2022/11/21 16:01:34 by afgoncal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,13 @@
 
 void	ft_putendl_fd(char *s, int fd)
 {
-	ft_putstr_fd(s, fd);
+	if (s)
+	{
+		while (*s)
+		{
+			ft_putchar_fd(*s, fd);
+			s++;
+		}
+	}
 	ft_putchar_fd('\n', fd);
 }
