52040c5659f7fb430b112e370c0761aaca07e619
Afonso Massena <afonsomassena@Afonsos-MacBook-Air.local>
2022-11-24 16:26:53 +0000

    wo_split

--- ft_striteri.c
+++ ft_striteri.c
@@ -6,10 +6,22 @@
 /*   By: afgoncal <massenaafonso1@gmail.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/23 15:01:13 by afgoncal          #+#    #+#             */
-/*   Updated: 2022/11/23 17:43:44 by afgoncal         ###   ########.fr       */
+/*   Updated: 2022/11/24 16:06:49 by afgoncal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/*#include "libft.h"
+#include "libft.h"
 
-void	ft_striteri(char *s, void (*f)(unsigned int, char*))*/
+void	ft_striteri(char *s, void (*f)(unsigned int, char *))
+{
+	unsigned int	index;
+
+	if (!s || !f)
+		return ;
+	index = 0;
+	while (s[index])
+	{
+		f(index, &s[index]);
+		index++;
+	}
+}
--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,10 +6,28 @@
 /*   By: afgoncal <massenaafonso1@gmail.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/23 15:00:43 by afgoncal          #+#    #+#             */
-/*   Updated: 2022/11/23 17:42:46 by afgoncal         ###   ########.fr       */
+/*   Updated: 2022/11/24 15:29:03 by afgoncal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/*#include "libft.h"
+#include "libft.h"
 
-char *ft_strmapi(char const *s, char (*f)(unsigned int, char));*/
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	char				*str;
+	unsigned int		index;
+
+	if (!s || !f)
+		return (NULL);
+	str = (char *)malloc(sizeof(char) * (ft_strlen(s) + 1));
+	if (!str)
+		return (NULL);
+	index = 0;
+	while (s[index])
+	{
+		str[index] = f(index, s[index]);
+		index++;
+	}
+	str[index] = '\0';
+	return (str);
+}
